0c75d8eebaa1cc3bcf95a3655b3330a50035c9c8
Zorayr Khalapyan <zkhalapyan@gmail.com>
2022-01-11 18:05:00 -0800

    Remove some unecessary files.

--- src/__tests__/minting-utils.test.ts
+++ src/__tests__/minting-utils.test.ts
@@ -1,4 +1,4 @@
-import { _getWarningMesssage } from '../minting-utils';
+import { _getWarningMesssage } from '../utils/minting-utils';
 
 test('Should get correct error message for code 311', () => {
   expect(_getWarningMesssage({code: 311, msg: " "})).toBe('All mints have been sold out.');
