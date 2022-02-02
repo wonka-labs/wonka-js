d3f1ff64bf20674c94a611bd76dde34b8e5a63ab
Zorayr Khalapyan <zkhalapyan@gmail.com>
2022-02-02 15:46:52 -0800

    [Example] Link mint to metadata page.

--- examples/next-example/pages/mints.tsx
+++ examples/next-example/pages/mints.tsx
@@ -7,11 +7,13 @@ import Button from '../components/Button';
 import { useConnection, useAnchorWallet, useWallet } from '@solana/wallet-adapter-react';
 import { Wonka, CandyMachineState } from '@triton-labs/wonka';
 import { Provider } from '@project-serum/anchor';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import { MetadataDataData } from '@metaplex-foundation/mpl-token-metadata';
 import { PublicKey } from '@solana/web3.js';
+import Link from 'next/link';
 
 interface Mint {
+  key: string;
   name: string;
   imageURL: string;
 }
@@ -28,21 +30,19 @@ const Mints: NextPage = () => {
   const router = useRouter();
 
   // 1. Create Wonka.
-  useEffect(() => {
-    if (anchorWallet && connection) {
+  useMemo(() => {
+    if (anchorWallet && connection && !wonka) {
       const provider = new Provider(connection, anchorWallet, {
         preflightCommitment: 'processed',
       });
       setWonka(new Wonka(provider, process.env.NEXT_PUBLIC_CANDY_MACHINE_ID!));
-    } else {
-      setWonka(null);
     }
-  }, [anchorWallet, connection]);
+  }, [anchorWallet, connection, wonka]);
 
   // 2. Fetch mints.
-  useEffect(() => {
+  useMemo(() => {
     async function fetchMints() {
-      if (wonka && !isFetching) {
+      if (wonka && !mints && !isFetching) {
         setIsFetching(true);
         console.log('Fetching mints...');
         const fetchedMints = await wonka.getCandyMachineMints();
@@ -50,7 +50,8 @@ const Mints: NextPage = () => {
           fetchedMints.map(async (mint) => {
             const data = await fetch(mint.data.data.uri);
             const dataJSON = await data.json();
-            return { name: dataJSON.name, imageURL: dataJSON.image };
+            console.log(mint.pubkey.toString());
+            return { key: mint.pubkey.toString(), name: dataJSON.name, imageURL: dataJSON.image };
           }),
         );
         setMints(fetchedMintsData);
@@ -58,14 +59,14 @@ const Mints: NextPage = () => {
       }
     }
     fetchMints();
-  }, [wonka, isFetching]);
+  }, [wonka, mints, isFetching]);
 
   function didTapBack() {
-    router.push('/mint')
+    router.push('/mint');
   }
 
   function didTapContinue() {
-    router.push('metadata');
+    router.push('/metadata');
   }
 
   return (
@@ -74,16 +75,20 @@ const Mints: NextPage = () => {
         <h1 className="text-5xl text-center">Fetching all Mints</h1>
         <p>
           Loading mints without using an indexer takes some time — give it a sec and we will load all existing minta via{' '}
-          <code>getCandyMachineMints(..).</code>
+          <code>getCandyMachineMints(..)</code>. Click on one of the mints to use <code>getMintMetadata(..)</code> to
+          fetch mint metadata.
         </p>
         {mints ? (
           <div className="grid grid-cols-1 md:grid-cols-4 gap-14 mt-10">
             {mints.map((mint, index) => {
+              console.log(`rendering mint with index: ${index}`);
               return (
-                <div key={mint.imageURL}>
-                  <Image width="208" height="208" src={mint.imageURL} alt={`wagmii profile photo.`} />
-                  <p className="text-2xl">{mint.name}</p>
-                </div>
+                <Link key={mint.key} href={`/metadata?address=${mint.key}`} passHref={true}>
+                  <div className='cursor-pointer'>
+                    <Image width="208" height="208" src={mint.imageURL} alt={`wagmii profile photo.`} />
+                    <p className="text-2xl">{mint.name}</p>
+                  </div>
+                </Link>
               );
             })}
           </div>
@@ -91,10 +96,10 @@ const Mints: NextPage = () => {
           <SpinnerCircularFixed color="white" secondaryColor="#515154" className="mx-auto mt-3" />
         )}
       </div>
-      <div className="flex flex-row space-x-3 my-10">
-        <Button title="< Back to Minting" didTapButton={didTapBack} />
-        <Button title="Continue to Mint Metadata >" didTapButton={didTapContinue} />
+      <div className="flex flex-row space-x-3 mt-10 mb-4">
+        <Button title="Back to Minting" didTapButton={didTapBack} />
       </div>
+      <p className='mb-10'>Tap on one of the mints to go to metadata page.</p>
     </div>
   );
 };
