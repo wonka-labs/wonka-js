e3793f43631684623d032a300fec99ce80d7b20c
Kunal Modi <kunalm@gmail.com>
2022-02-11 14:44:33 -0800

    [Upload] expose public upload endpoint for other types of files

--- src/arweave-uploader.ts
+++ src/arweave-uploader.ts
@@ -30,32 +30,23 @@ export default class ArweaveUploader {
 
   public async uploadJSON(json: any) {
     const buf = Buffer.from(JSON.stringify(json));
+    return await this.uploadBuffer(buf, 'application/json', 'json');
+  }
+
+  public async uploadBase64PNG(b64string: string) {
+    const buf = Buffer.from(_sanitizeB64String(b64string), 'base64');
+    return await this.uploadBuffer(buf, 'image/png', 'png');
+  }
+
+  public async uploadBuffer(buf: ArrayBufferLike, mimeType: string, arweaveExt?: string): Promise<string> {
+    console.log('uploadBuffer', mimeType, arweaveExt);
     const transaction = await this._arweave.createTransaction({ data: buf }, this._arweaveKey);
-    transaction.addTag('Content-Type', 'application/json');
+    transaction.addTag('Content-Type', mimeType);
     await this._arweave.transactions.sign(transaction, this._arweaveKey);
     const response = await this._arweave.transactions.post(transaction);
     const status = await this._arweave.transactions.getStatus(transaction.id)
     log.info(`Completed transaction ${transaction.id} with status code ${status.status}!`)
-    return `https://www.arweave.net/${transaction.id}?ext=json`
-  }
-
-  public async uploadBase64PNG(b64string: string) {
-    // #1 Convert base64 string to a binary data buffer.
-    const buf = Buffer.from(_sanitizeB64String(b64string), 'base64');
-
-    // #4 Check out wallet balance. We should probably fail if too low? 
-    const arweaveWallet = await this._arweave.wallets.jwkToAddress(this._arweaveKey);
-    const arweaveWalletBallance = await this._arweave.wallets.getBalance(arweaveWallet);
-    
-    // #5 Core flow: create a transaction, upload and wait for the status! 
-    const transaction = await this._arweave.createTransaction({ data: buf }, this._arweaveKey);
-    transaction.addTag('Content-Type', 'image/png');
-    await this._arweave.transactions.sign(transaction, this._arweaveKey);
-    const response = await this._arweave.transactions.post(transaction);
-    const status = await this._arweave.transactions.getStatus(transaction.id)
-    log.info(`Completed transaction ${transaction.id} with status code ${status}!`)
-
-    // #6 This is the tricky part, use the format below to get to the PNG url! 
-    return `https://www.arweave.net/${transaction.id}?ext=png`
+    const ext = arweaveExt ? `?ext=${encodeURIComponent(arweaveExt)}` : ''
+    return `https://www.arweave.net/${transaction.id}${ext}`
   }
 }
