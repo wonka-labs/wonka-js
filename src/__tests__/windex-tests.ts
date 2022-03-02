4146a7d3a6980ec25e87762732af5edbb8d9c91f
Kunal Modi <kunalm@gmail.com>
2022-03-01 17:34:44 -0800

    [windex] update queries, add mainnet/devnet switch

--- src/__tests__/windex-tests.ts
+++ src/__tests__/windex-tests.ts
@@ -1,4 +1,4 @@
-import { Windex } from '../windex';
+import { Windex, WINDEX_ENDPOINT } from '../windex';
 import {PublicKey} from '@solana/web3.js'
 
 const testCandyMachineId = new PublicKey("Hkunn4hct84zSPNpyQygThUKn8RUBVf5b4r975NRaHPb")
@@ -15,7 +15,7 @@ test('should be able to fetch first candy machine NFT', () => {
   return Windex.fetchNFTsByCandyMachine(testCandyMachineId, 2).then(results => {
     expect(results[0].address).toBeDefined()
     expect(results[0].name).toBeDefined()
-    expect(results[0].uri).toBeDefined()
+    expect(results[0].image_url).toBeDefined()
   });
 }, 10000);
 
@@ -24,7 +24,16 @@ test('should be able to fetch NFT by Wallet', () => {
   return Windex.fetchNFTsByWallet(testWalletId, 2).then(results => {
     expect(results[0].address).toBeDefined()
     expect(results[0].name).toBeDefined()
-    expect(results[0].uri).toBeDefined()
+    expect(results[0].image_url).toBeDefined()
+  });
+}, 10000);
+
+test('should be able to fetch NFT by Wallet (mainnet)', () => {
+  const testWalletId = new PublicKey("BYeHCJtokQecDkN34ZE4fWgF7U4vDtwjX6bkaiaprQmt")
+  return Windex.fetchNFTsByWallet(testWalletId, 2, WINDEX_ENDPOINT.MAINNET).then(results => {
+    expect(results[0].address).toBeDefined()
+    expect(results[0].name).toBeDefined()
+    expect(results[0].image_url).toBeDefined()
   });
 }, 10000);
 
@@ -33,6 +42,6 @@ test('should be able to fetch NFT by ID', () => {
   return Windex.fetchNFTByMintAddress(testNFTId).then(results => {
     expect(results!.address).toBe("GsuMovmB1rrYqHeTvoheAr8LFJTLktqrr6U2hZRvKDSj");
     expect(results!.name).toBe("Wagmify PFP 45");
-    expect(results!.uri).toBe("https://www.arweave.net/p0zxL9xdNDA_lcRCTOiTCISa4hnG19GDQIjulEiIUNs?ext=png");
+    expect(results!.image_url).toBe("https://www.arweave.net/p0zxL9xdNDA_lcRCTOiTCISa4hnG19GDQIjulEiIUNs?ext=png");
   });
 }, 10000);
--- src/windex.ts
+++ src/windex.ts
@@ -2,42 +2,91 @@ import { request, gql } from 'graphql-request';
 import { PublicKey } from '@solana/web3.js';
 import log from 'loglevel';
 
-// Also can use visual explorer called GraphiQL:
-// 'https://windex-api.onrender.com/api/v1/devnet/graphiql';
-const WINDEX_ENDPOINT = 'https://windex-api.onrender.com/api/v1/devnet/graphql';
+export enum WINDEX_ENDPOINT {
+  // Solana Devnet (https://explorer.solana.com/?cluster=devnet)
+  // To explore queries: https://index.wonkalabs.xyz/api/v1/devnet/graphiql
+  DEVNET = "https://index.wonkalabs.xyz/api/v1/devnet/graphql",
 
-export interface Collection {
-  candyMachine: CandyMachineState;
-  items: CollectionItem[];
+  // Solana Mainnet Beta (https://explorer.solana.com/)
+  // To explore queries: https://index.wonkalabs.xyz/api/v1/mainnet/graphiql
+  MAINNET = "https://index.wonkalabs.xyz/api/v1/mainnet/graphql",
 }
 
 export interface CandyMachineState {
+  id: string;
   items_redeemed: number;
   items_available: number;
   price: number;
+  go_live_date: number;
 }
 
 export interface CollectionItem {
   address: string;
   name: string;
-  uri: string;
+  symbol: string | null;
+  description: string | null | undefined;
+  external_url: string | null | undefined;
+  image_url: string;
+  explorer_url: string;
+  creators: {
+    address: string;
+    verified: boolean;
+    share: number;
+  }[],
+  files: {
+    uri: string | null;
+    type: string | null;
+  }[],
+  attributes: {
+    trait_type: string | null;
+    value: string | null;
+  }[],
 }
 
 type nft = {
+  id: string;
   name: string;
-  metaplex_metadata: {
-    mint: string;
-  };
+  symbol: string | null;
+  explorer_url: string;
   image: {
     orig: string;
   };
+  metaplex_metadata: {
+    mint: string;
+    creators: {
+      address: string;
+      verified: boolean;
+      share: number;
+    }[],
+  };
+  external_metadata: {
+    description: string | null;
+    external_url: string | null;
+    properties: {
+      files: {
+        uri: string | null;
+        type: string | null;
+      }[] | null;
+    } | null;
+    attributes: {
+      trait_type: string | null;
+      value: string | null;
+    }[] | null;
+  } | null;
 };
 
 function nftToCollectionItem(nft: nft): CollectionItem {
   return {
     address: nft.metaplex_metadata.mint,
     name: nft.name,
-    uri: nft.image.orig,
+    symbol: nft.symbol,
+    description: nft.external_metadata?.description,
+    external_url: nft.external_metadata?.external_url,
+    image_url: nft.image.orig,
+    explorer_url: nft.explorer_url,
+    creators: nft.metaplex_metadata.creators,
+    files: nft.external_metadata?.properties?.files || [],
+    attributes: nft.external_metadata?.attributes || [],
   };
 }
 
@@ -56,23 +105,29 @@ function nftsToCollectionItems(nfts: nft[]): CollectionItem[] {
  * - NFT by Mint Address
  */
 export default class Windex {
-  public static async fetchCandyMachineState(candyMachineId: PublicKey): Promise<CandyMachineState> {
+  public static async fetchCandyMachineState(
+    candyMachineId: PublicKey,
+    endpoint: WINDEX_ENDPOINT = WINDEX_ENDPOINT.DEVNET,
+  ): Promise<CandyMachineState> {
     log.info(`Fetching candy machine state for candy machine with ID: ${candyMachineId.toString()}`);
     const fetchCandyMachineStateQuery = gql`
     {
       candyMachineV2(id: "${candyMachineId.toString()}") {
+        id
         items_redeemed
         items_available
         price
+        go_live_date
       }
     }`;
-    const results = await request(WINDEX_ENDPOINT, fetchCandyMachineStateQuery);
+    const results = await request(endpoint, fetchCandyMachineStateQuery);
     return results.candyMachineV2 as CandyMachineState;
   }
 
   public static async fetchNFTsByCandyMachine(
     candyMachineId: PublicKey,
-    first: number = 128,
+    first: number = 20,
+    endpoint: WINDEX_ENDPOINT = WINDEX_ENDPOINT.DEVNET,
   ): Promise<CollectionItem[]> {
     log.info(`Fetching NFTs by candy machine with ID: ${candyMachineId.toString()}`);
     const fetchNFTsByCandyMachineQuery = gql`
@@ -80,60 +135,133 @@ export default class Windex {
       nftsByCollection(collectionId:"${candyMachineId.toString()}", first:${first}) {
         edges {
           node {
-            metaplex_metadata {
-              mint
-            }
+            id
             name
+            symbol
+            explorer_url
             image {
               orig
+            }
+            metaplex_metadata {
+              mint
+              creators {
+                address
+                verified
+                share
+              }
+            }
+            external_metadata {
+              description
+              external_url
+              properties {
+                files {
+                  uri
+                  type
+                }
+              }
+              attributes {
+                trait_type
+                value
+              }
             }
           }
         }
       }
     }`;
-    const results = await request(WINDEX_ENDPOINT, fetchNFTsByCandyMachineQuery);
+    const results = await request(endpoint, fetchNFTsByCandyMachineQuery);
     const nfts = results.nftsByCollection.edges.map((edge) => edge.node) as nft[];
     return nftsToCollectionItems(nfts);
   }
 
-  public static async fetchNFTsByWallet(walletAddress: PublicKey, first: number = 128): Promise<CollectionItem[]> {
+  public static async fetchNFTsByWallet(
+    walletAddress: PublicKey,
+    first: number = 20,
+    endpoint: WINDEX_ENDPOINT = WINDEX_ENDPOINT.DEVNET,
+  ): Promise<CollectionItem[]> {
     log.info(`Fetching NFTs by wallet with ID: ${walletAddress.toString()}`);
     const fetchNFTsByCandyMachineQuery = gql`
     {
       nftsByWallet(wallet: "${walletAddress.toString()}", first: ${first}) {
         edges {
           node {
-            metaplex_metadata {
-              mint
-            }
+            id
             name
+            symbol
+            explorer_url
             image {
               orig
+            }
+            metaplex_metadata {
+              mint
+              creators {
+                address
+                verified
+                share
+              }
+            }
+            external_metadata {
+              description
+              external_url
+              properties {
+                files {
+                  uri
+                  type
+                }
+              }
+              attributes {
+                trait_type
+                value
+              }
             }
           }
         }
       }
     }`;
-    const results = await request(WINDEX_ENDPOINT, fetchNFTsByCandyMachineQuery);
+    const results = await request(endpoint, fetchNFTsByCandyMachineQuery);
     const nfts = results.nftsByWallet.edges.map((edge) => edge.node) as nft[];
     return nftsToCollectionItems(nfts);
   }
 
-  public static async fetchNFTByMintAddress(mintAddress: PublicKey): Promise<CollectionItem | null> {
+  public static async fetchNFTByMintAddress(
+    mintAddress: PublicKey,
+    endpoint: WINDEX_ENDPOINT = WINDEX_ENDPOINT.DEVNET,
+  ): Promise<CollectionItem | null> {
     log.info(`Fetching NFTs by mint address with ID: ${mintAddress.toString()}`);
     const fetchNFTsByCandyMachineQuery = gql`
     {
       nftByMintID(mintID: "${mintAddress.toString()}") {
-        metaplex_metadata {
-          mint
-        }
+        id
         name
+        symbol
+        explorer_url
         image {
           orig
         }
+        metaplex_metadata {
+          mint
+          creators {
+            address
+            verified
+            share
+          }
+        }
+        external_metadata {
+          description
+          external_url
+          properties {
+            files {
+              uri
+              type
+            }
+          }
+          attributes {
+            trait_type
+            value
+          }
+        }
       }
     }`;
-    const results = await request(WINDEX_ENDPOINT, fetchNFTsByCandyMachineQuery);
+    const results = await request(endpoint, fetchNFTsByCandyMachineQuery);
     return nftToCollectionItem(results.nftByMintID);
   }
 }
