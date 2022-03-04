e11c658e21baff4aac4b6d80f2a135829d3a85a4
Kunal Modi <kunalm@gmail.com>
2022-03-03 17:59:16 -0800

    [mint] more descriptive warning message for candy machine errors

--- src/__tests__/minting-utils.test.ts
+++ src/__tests__/minting-utils.test.ts
@@ -1,5 +1,7 @@
 import { _getWarningMesssage } from '../utils/minting-utils';
 
-test('Should get correct error message for code 311', () => {
-  expect(_getWarningMesssage({ code: 311, msg: ' ' })).toBe('All mints have been sold out.');
+test('Should get correct error message for not enough sol error', () => {
+  expect(_getWarningMesssage({
+    message: 'error SendTransactionError: failed to send transaction: Transaction simulation failed: Error processing Instruction 4: custom program error: 0x1778',
+  })).toBe('Not enough SOL to pay for this minting');
 });
